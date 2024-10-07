4b3d7814f3448546ecbf3985a79c3b46580ac379
Gabriel Rocha <111160701+GabrielRoch4@users.noreply.github.com>
2024-10-07 09:31:32 -0300

    Validação contador negativo

--- app/src/main/java/com/example/mycontadorjava/MainActivity.java
+++ app/src/main/java/com/example/mycontadorjava/MainActivity.java
@@ -1,24 +1,49 @@
 package com.example.mycontadorjava;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private Button btnAumentar;
+    private Button btnDiminuir;
+    private int contador = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        textView = findViewById(R.id.textView);
+        btnAumentar = findViewById(R.id.activity_main_btn_aumentar);
+        btnDiminuir = findViewById(R.id.activity_main_btn_diminuir);
+
+        btnDiminuir.setEnabled(false);
+
+        btnAumentar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                contador++;
+                textView.setText(String.valueOf(contador));
+                btnDiminuir.setEnabled(true);
+            }
+        });
+
+        btnDiminuir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (contador > 0) {
+                    contador--;
+                    textView.setText(String.valueOf(contador));
+                    if (contador == 0) {
+                        btnDiminuir.setEnabled(false);
+                    }
+                }
+            }
         });
     }
 }
